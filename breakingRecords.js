1ead14db31fa09881d0d5ef65f032431c8b0a53c
Brian Short <brian.ralph.short@gmail.com>
2021-07-06 11:15:02 -0400

    Add breaking records algo review

---
+++ breakingRecords.js
@@ -0,0 +1,18 @@
+function breakingRecords(scores) {
+    // Write your code here
+    let maxScore = 0;
+    let minScore = Infinity;
+    let maxCount = -1;
+    let minCount = -1;
+    for (var i=0; i < scores.length; i++) {
+        if (scores[i] < minScore) {
+            minCount++;
+        }
+        if (scores[i] > maxScore) {
+            maxCount++;
+        }
+        maxScore = Math.max(maxScore, scores[i]);
+        minScore = Math.min(minScore, scores[i]);
+    }
+    return [ maxCount, minCount ];
+}
