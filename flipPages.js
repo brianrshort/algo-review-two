083905c31460dd1ac2b8983a52ca4b80f72daaf4
Brian Short <brian.ralph.short@gmail.com>
2021-07-13 12:46:40 -0400

    Update algo

--- flipPages.js
+++ flipPages.js
@@ -1,12 +1,9 @@
 function pageCount(n, p) {
     // Write your code here
+
     let count = 0;
-    if (0 + p < n - p) {
-        count = 0 + p;
-    } else {
-        count = n - p;
-    }
-    console.log(count);
-    count = Math.floor(count / 2);
-    return count;
+    let front = Math.floor(p / 2);
+    let back = Math.floor(n / 2 - front);
+    return Math.min(front, back);
+
 }
