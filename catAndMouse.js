02ffb81d03d8c2ece10538b40f483805c96375ca
Brian Short <brian.ralph.short@gmail.com>
2021-07-13 12:46:50 -0400

    Add cat and mouse algo review

---
+++ catAndMouse.js
@@ -0,0 +1,11 @@
+function catAndMouse(x, y, z) {
+    let catA = Math.abs(x - z);
+    let catB = Math.abs(y - z);
+    if (catA < catB) {
+        return "Cat A";
+    } else if (catA > catB) {
+        return "Cat B";
+    } else {
+        return "Mouse C";
+    }
+}
